a9ef78ce99b8213d89d1e93bb65d3c1549b29367
jashvanth370 <jashva25@gmail.com>
2025-01-09 10:38:40 +0530

    update

--- BookStore/src/main/java/com/BookStore/BookStore/BookStoreApplication.java
+++ BookStore/src/main/java/com/BookStore/BookStore/BookStoreApplication.java
@@ -1,6 +1,6 @@
 package com.BookStore.BookStore;
 
-import org.springframework.boot.SpringApplication;ook
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
